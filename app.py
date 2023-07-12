40f4596d8e0583c494acb91de9b268d43efbcf56
Juan Munoz <j.munoz@lacroix.group>
2023-07-12 15:23:24 -0600

    Endpoints requests modified to use stores and items ids. Untested.

--- app.py
+++ app.py
@@ -1,65 +1,49 @@
 from flask import Flask, request
+from db import items, stores
+import uuid
 
 app = Flask(__name__)
 
-# In this version, stores will be stored in a dictionary.  
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
 # Endpoint to return the stores list and their items
-@app.get("/store") # http://127.0.0.1:5000/store
+@app.get("/stores") # http://127.0.0.1:5000/store
 # When the user types this address in the browser, the `get_store()` method is executed 
 # and return the list of the stores.
-def get_store():
-    return {"stores": stores}
+def get_stores():
+    return {"stores": list(stores.values())}
 
 # Endpoint to create a store from the browser client.
 @app.post("/store")
 def create_store():
-    request_data = request.get_json() # Retrieve the json sent by the browser client.
-    new_store = {"name": request_data["name"], "items": []} # json parsed into the new_store object.
-    stores.append(new_store) # New store json values appended to the stores dictionary.
-    return new_store, 201 # Return the new store and an http status that ok, accepted.
+    store_data = request.get_json() # Retrieve the json sent by the browser client.
+    store_id = uuid.uuid4().hex # Just an hexadecimal random id
+    store ={**store_data, "id": store_id} # Based on json data create a new dictionary element with id
+    stores[store_id] = store # New store json values appended to the stores dictionary.
+    return store, 201 # Return the new store and an http status that ok, accepted.
 
-# Endpoint to create an items of a store from the browser client.
-@app.post("/store/<string:name>/item")
+# Endpoint to create an item of a store from the browser client.
+@app.post("/item")
 def create_item(name):
-    request_data = request.get_json() # Retrieve the json sent by the browser client.
-    # Loop (sweep) the dictionary for the store name to match 
-    # so the item is appended.
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    
-    return {"message": "Store not found"}, 404
+    item_data = request.get_json() # Retrieve the json sent by the browser client.
+    # Verify if the store_id sent exists in the dictionary. If not, returns and error message. 
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
 
-# Endpoint to get the store by searching from the browser client.
-@app.post("/store/<string:name>")
-def get_store1(name):
-    # Loop (sweep) the dictionary for the store name to match.
-    for store in stores:
-        if store["name"] == name:
-            return store, 201 # Return the store.
+    # If store_id exists, create the item.
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
     
-    return {"message": "Store not found"}, 404
+    return item, 201
 
-# Endpoint to get the store by searching from the browser client.
-@app.post("/store/<string:name>")
-def get_item_in_store(name):
-    # Loop (sweep) the dictionary for the store name to match.
-    for store in stores:
-        if store["name"] == name:
-            return {store["items"]}, 201 # Return the item.
-    
-    return {"message": "Store or item not found"}, 404     
+# Endpoint to retrieve all items
+@app.get("/item") 
+def get_all_items():
+    return {"items": list(items.values())}
+
+# Endpoint to get and item in dictionary.
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id] # Return the store based on id.
+    except KeyError:
+        return {"message": "Item not found"}, 404
