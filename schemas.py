946b78567fe6b689ad9d0810da9b932673f0e2e7
Juan Munoz <j.munoz@lacroix.group>
2023-07-15 20:46:02 -0600

    Code with error on models import from app.py

--- schemas.py
+++ schemas.py
@@ -1,16 +1,25 @@
 from marshmallow import Schema, fields
 
-
-class ItemSchema(Schema):
+# Include a nested item within a store
+# but do not want to add information about the store itself
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
     store_id = fields.Str(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
     
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+
+# Nested fields    
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
     
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)    
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))    
