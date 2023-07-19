a1f5cd7dfcf87f7088d6b1911aa39a0e90378936
Juan Munoz <j.munoz@lacroix.group>
2023-07-18 22:23:48 -0600

    JWT protected endpoints and logout implemented. Tested and working awesomely flawless.

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask import Flask, jsonify
 
 from db import db
+from blocklist import BLOCKLIST
 
 import models
 
@@ -36,6 +38,54 @@ def create_app(db_url=None):
     
     app.config["JWT_SECRET_KEY"] = "jose" # 337538490824582634302402106023111532003
     jwt = JWTManager(app)
+    
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )    
+    
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )    
     
     with app.app_context():
         db.create_all()
